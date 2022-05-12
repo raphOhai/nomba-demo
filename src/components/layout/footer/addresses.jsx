20e627510476c5d5864fb22a901a05ebfbed8d07
FourthCanvas <63639530+fourthcanvas@users.noreply.github.com>
2022-05-12 12:53:49 +0100

    Policy Page and Updates (#157)
    * Ao policy pages fix (#156)
    
    * ao_fix: add svg shapes to policy page header
    
    * ao_feat: navigation of terms submenu
    
    * ao_chore: remove unused items in policies.json
    
    * fix flag issue (#154)
    
    * fix flag issue
    
    * resolve comment
    
    * ao_chore: remove gatsby-ssr.js
    
    * move classname above to remove cli warning
    
    Co-authored-by: Aremu Smog <aremuoluwagbamila@gmail.com>
    
    Co-authored-by: Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
    Co-authored-by: Chineta Adinnu <52791481+Netacci@users.noreply.github.com>

--- src/components/layout/footer/addresses.jsx
+++ src/components/layout/footer/addresses.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ctl from "@netlify/classnames-template-literals";
 
-import addressList from "config/addresses.json";
+import { addressList } from "config/addresses.js";
 import { Ntext } from "../../ntext";
 
 const Addresses = () => {
-  const addressListKeys = Object.keys(addressList);
   return (
     <div className={addressesWrapperStyle}>
-      {addressListKeys.map(addressListKey => (
-        <Address key={addressListKey} location={addressList[addressListKey]} />
+      {addressList.map((addressListKey, index) => (
+        <Address key={`addressListKey_${index}`} {...addressListKey} />
       ))}
     </div>
   );
@@ -21,8 +20,17 @@ md:grid-cols-2
 max-w-[1000px]
 gap-4
 `);
-const Address = ({ location }) => {
-  return <Ntext variant="p14">{location}</Ntext>;
+const Address = ({ location, flag }) => {
+  return (
+    <>
+      <div className="flex ">
+        {flag}
+        <Ntext variant="p14" className="ml-2">
+          {location}
+        </Ntext>
+      </div>
+    </>
+  );
 };
 
 export { Addresses };
