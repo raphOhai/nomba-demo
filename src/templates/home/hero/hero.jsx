013bf39683fbd94698b67902947821eb006634f5
Johnexzy <obajohn75@gmail.com>
2023-03-31 22:39:52 +0100

    ui(hero): updated hero section with new ui
    pending mobile responsiveness

--- src/templates/home/hero/hero.jsx
+++ src/templates/home/hero/hero.jsx
@@ -11,15 +11,16 @@ const HomepageHero = () => {
     <section className={heroStyle}>
       <Container>
         <div className={heroTextWrapperStyle}>
-          <Ntext variant="h1" color="primary-1000">
-            Get better at business <Br on="desktop" /> with Nomba.
-          </Ntext>
+          <div className={heroTextHeaders}>
+            <Ntext variant="h1" color="primary-1000" className="basis-2/3">
+              Get better at business <Br on="desktop" /> with Nomba.
+            </Ntext>
 
-          <Ntext variant="p18" className={subTextStyle}>
-            Over <span className={coloredText}>235,000</span> businesses trust Nomba to securely accept card
-            and transfer payments, manage their money, and grow their
-            business.
-          </Ntext>
+            <Ntext variant="p18" className={subTextStyle}>
+              Over 235,000 businesses trust Nomba to securely accept card and
+              transfer payments, manage their money, and grow their business.
+            </Ntext>
+          </div>
           <div className={heroButtonsContainer}>
             <Button
               href={{ url: SIGNUP_URL }}
@@ -29,46 +30,34 @@ const HomepageHero = () => {
             <ReadMore
               color="n-grey6"
               variant="text3"
-              href={{ "url": "tel:+23401888899" }}
+              href={{ url: "tel:+23401888899" }}
               text="Contact sales"
-
             />
           </div>
         </div>
         <div className={imageGridStyle}>
-          <div className={imageWrapperStyle}></div>
           <div className={imageWrapperStyle}>
             <StaticImage
-              width={510}
-              height={406}
-              src="../../../assets/images/jpegs/homepage/POS agent - nomba.jpg"
-              style={{
-                clipPath: "polygon(0% 99%, 0 68%, 86% 0, 100% 0, 100% 100%)",
-              }}
+              width={397}
+              height={387}
+              src="../../../assets/images/jpegs/homepage/hero-images/hero1.jpg"
               alt="Nomba POS Agent"
             />
           </div>
-          <div className={`${imageWrapperStyle} !block`}>
+          <div className={imageWrapperStyle}>
             <StaticImage
-              width={746}
-              height={600}
-              src="../../../assets/images/jpegs/homepage/dfrte.png"
-              style={{
-                clipPath:
-                  "polygon(0 0, 100% 0, 100% 50%, 33% 50%, 23% 66%, 0 66%)",
-              }}
+              width={397}
+              height={387}
+              src="../../../assets/images/jpegs/homepage/hero-images/hero2.jpg"
               alt="Woman at the closing store."
             />
           </div>
 
           <div className={imageWrapperStyle}>
             <StaticImage
-              width={510}
-              height={459}
-              src="../../../assets/images/jpegs/homepage/market woman - nomba.jpg"
-              style={{
-                clipPath: "inset(0% 0% 31%)",
-              }}
+              width={397}
+              height={387}
+              src="../../../assets/images/jpegs/homepage/hero-images/hero3.jpg"
               alt="Woman making payment over POS"
             />
           </div>
@@ -94,30 +83,34 @@ md:max-w-[605px]
 max-w-[100%]
 lg:mb-[80px]
 sm:mb-[120px]
-mb-12
+mb-10
+`);
+
+const heroTextHeaders = ctl(`
+flex
+flex-col
+md:flex-row
+mb-10
 `);
 
 const subTextStyle = ctl(`
-lg:max-w-[605px]
-max-w-[405px]
 mb-8
 mt-6
+basis-1/3
 `);
 
 const imageWrapperStyle = ctl(`
-md:block
+rounded 
+md:basis-1/3
 hidden
-odd:col-span-3
-even:col-span-2
+md:block
+
 `);
 const imageGridStyle = ctl(`
-grid
-md:grid-cols-5
-grid-cols-1
-gap-[40px]
-lg:-mt-[450px]
-md:-mt-[350px]
+flex flex-row
 pointer-events-none
+mb-10
+gap-10
 `);
 
 const heroButtonsContainer = ctl(`
