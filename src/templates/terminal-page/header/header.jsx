79b8c5ac6486a047782045d0b02432d2e8c4a4f2
Jesse <akohgodwinjesse@gmail.com>
2023-04-07 17:06:07 +0100

    adjust mobile view

--- src/templates/terminal-page/header/header.jsx
+++ src/templates/terminal-page/header/header.jsx
@@ -5,22 +5,20 @@ import "./index.scss";
 const TerminalHeader = () => {
   return (
     <header className="terminal_header">
-      <div className="terminal_header_wrap">
-        <Logo />
-        <Button text="Join the waitlist" size="xsmall" />
-        <div className="cta">
-          <p>Join the waitlist</p>
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M5.93994 13.28L10.2866 8.9333C10.7999 8.41997 10.7999 7.57997 10.2866 7.06664L5.93994 2.71997"
-              stroke="#FFCC00"
-              strokeWidth="1.5"
-              strokeMiterlimit="10"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
+      <Logo />
+      <Button text="Join the waitlist" size="xsmall" />
+      <div className="cta">
+        <p>Join the waitlist</p>
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M5.93994 13.28L10.2866 8.9333C10.7999 8.41997 10.7999 7.57997 10.2866 7.06664L5.93994 2.71997"
+            stroke="#FFCC00"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
     </header>
   );
--- src/templates/terminal-page/terminal-sect/terminalSectInt.jsx
+++ src/templates/terminal-page/terminal-sect/terminalSectInt.jsx
@@ -86,7 +86,7 @@ const TerminalSectionInteractions = () => {
             speed printer, durable and reliable POS terminal
           </p>
           <div className="cta">
-            <p>Join the waitlist</p>
+            <p>Join the experience</p>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M5.93994 13.28L10.2866 8.9333C10.7999 8.41997 10.7999 7.57997 10.2866 7.06664L5.93994 2.71997"
@@ -102,7 +102,7 @@ const TerminalSectionInteractions = () => {
       </div>
       <div className="c_terminal_sectInt_print child_wrap text-center">
         <h1 data-animation="h">
-          Get ahead with the power of <Br on="all" /> Nomba MAX!
+          Get ahead with the power of <Br on="desktop" /> Nomba MAX!
         </h1>
         <div className="c_terminal_sectInt_print_fl">
           <video autoPlay muted src={TerminalPrint}></video>
