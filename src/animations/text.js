ce07ee3e59e3e235a540ca7f40f6d88b128b41a0
Jesse <akohgodwinjesse@gmail.com>
2023-04-09 22:52:29 +0100

    backup text animation since i cant configure split doesnt work with  gatsby ssr

--- src/animations/text.js
+++ src/animations/text.js
@@ -1,74 +1,10 @@
-import Splitting from "splitting";
-import { IO } from "./observe";
-import "splitting/dist/splitting.css";
-import "splitting/dist/splitting-cells.css";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { IO } from "./observe";
 gsap.registerPlugin(ScrollTrigger);
 
 export const split = () => {
-  const p = document.querySelectorAll("[data-animation='p']");
-  const H = document.querySelectorAll("[data-animation='h']");
-  const f = document.querySelectorAll("[data-animation='fadeInOut']");
-  p.forEach(item => {
-    const line = Splitting({
-      target: item,
-      by: "lines",
-    });
-    line.forEach(splitResult => {
-      const wrappedLines = splitResult.words
-        .map(
-          wordsArr => `
-                       <span class="word_wrap">
-                             ${wordsArr.outerHTML}
-                        </span>`
-        )
-        .join("");
-      splitResult.el.innerHTML = wrappedLines;
-    });
-
-    gsap.set(item.querySelectorAll(".word"), {
-      yPercent: 105,
-      opacity: 0,
-      rotateX: 50,
-      transformStyle: "preserve-3d",
-    });
-    IO(item, { threshold: 0.7 }).then(() => {
-      const elem = item.querySelectorAll(".word");
-      gsap.to(elem, {
-        yPercent: 0,
-        opacity: 1,
-        rotateX: 0,
-        stagger: elem.length > 100 ? 0.02 : 0.03,
-        duration: elem.length > 100 ? 0.5 : 0.6,
-        ease: "easeOut",
-      });
-    });
-  });
-
-  H.forEach(item => {
-    Splitting({
-      target: item,
-      by: "chars",
-    });
-    gsap.set(item.querySelectorAll(".char"), {
-      opacity: 0,
-      yPercent: 100,
-      transformStyle: "preserve-3d",
-    });
-    IO(item, {
-      threshold: 1,
-    }).then(() => {
-      const elem = item.querySelectorAll(".char");
-      gsap.to(elem, {
-        opacity: 1,
-        yPercent: 0,
-        stagger: elem.length > 100 ? 0.005 : 0.01,
-        duration: elem.length > 100 ? 0.4 : 0.5,
-        ease: "easeOut",
-      });
-    });
-  });
+  const f = document.querySelectorAll("[data-animation='h']");
 
   f.forEach(item => {
     gsap.set(item, {
@@ -76,19 +12,16 @@ export const split = () => {
       yPercent: 50,
       transformStyle: "preserve-3d",
     });
-    gsap.to(item, {
-      autoAlpha: 1,
-      yPercent: 0,
-      duration: item.length > 100 ? 0.7 : 0.6,
-      ease: "easeOut",
-      scrollTrigger: {
-        trigger: item,
-        start: "top center",
-        end: "+=300",
-        scrub: true,
-        ease: "circ.easeOut",
-        toggleAttribute: "play play reverse reverse",
+    IO(item).then(
+      () => {
+        gsap.to(item, {
+          autoAlpha: 1,
+          yPercent: 0,
+          duration: item.length > 100 ? 0.7 : 0.6,
+          ease: "easeOut",
+        });
       },
-    });
+      { threshold: 1 }
+    );
   });
 };
