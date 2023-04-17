227eb4c41176319219ff976ff318847044eec8f9
John Oba <obajohn75@gmail.com>
2023-04-17 13:13:00 +0100

    fix(testimonials): added new testimonials

--- src/config/testimonials/small-business.js
+++ src/config/testimonials/small-business.js
@@ -4,26 +4,26 @@ import { imageStyle } from "./testimonial-image-style";
 
 const SmallBusinessTestimonials = [
   {
-    name: "Azoronyeahu Uchenna Gentle",
-    role: "Akjfintech Solutions Global Resources",
+    name: "Auwal Usman",
+    role: "CEO, Auwalus Business Concept",
     testimonial:
       "Nomba (Formerly Kudi) is loved here in Adamawa State and the North because Kudi means money in Hausa and with the Money means Numbers (Nomba).",
     image: (
       <StaticImage
-        src="../../assets/images/jpegs/small-business/auwal-usman.png"
-        alt="Azoronyeahu Uchenna Gentle"
+        src="../../assets/images/jpegs/small-business/auwal-usman-1.png"
+        alt="Auwal Usman"
         className={imageStyle}
       />
     ),
   },
   {
-    name: "Mrs Grace Uchenna",
-    role: "Solutions Global Resources",
+    name: "Goddey Duke",
+    role: "",
     testimonial:
-      "Nomba (Formerly Kudi) is loved here in Adamawa State and the North because Kudi means money in Hausa and with the Money means Numbers (Nomba).",
+      "In 2018/2019, I was introduced to Nomba (Formerly Kudi), and joining Nomba gave my business a new lease on life. Because I can cash out my funds immediately after a withdrawal, my capital has increased. In June 2021 I became a Nomba partner and in less than a year my business capital has increased 10X.",
     image: (
       <StaticImage
-        src="../../assets/images/jpegs/small-business/woman-smiling.png"
+        src="../../assets/images/jpegs/small-business/Goddey Duke.jpg"
         alt="Mrs Grace"
         className={imageStyle}
       />
