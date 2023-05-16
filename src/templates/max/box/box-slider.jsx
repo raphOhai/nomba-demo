7777d97e5788e8b77d522a809e878526058b9158
John Oba <obajohn75@gmail.com>
2023-05-16 11:44:02 +0100

    miscellaneous fixes

--- src/templates/max/box/box-slider.jsx
+++ src/templates/max/box/box-slider.jsx
@@ -15,11 +15,11 @@ import PreviousIconDark from "svgs/chevron-left.svg";
 SwiperCore.use([Navigation, Pagination]);
 
 const BoxSlider = ({ slides }) => {
-  const sliderItems = slides.map(item => (
-    <SwiperSlide key={item.id}>
-      <Box title={item.title} image={item.image} key={item.id} />
-    </SwiperSlide>
-  ));
+  // const sliderItems = slides.map(item => (
+  //   <SwiperSlide key={item.id}>
+  //     <Box title={item.title} image={item.image} key={item.id} />
+  //   </SwiperSlide>
+  // ));
 
   return (
     <>
@@ -40,7 +40,25 @@ const BoxSlider = ({ slides }) => {
             slidesPerView={1}
             breakpoints={breakpoints}
           >
-            {sliderItems}
+            {/* {sliderItems} */}
+            <SwiperSlide>
+              <Box title={slides[0].title} image={slides[0].image} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box title={slides[1].title} image={slides[1].image} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box title={slides[2].title} image={slides[2].image} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box title={slides[3].title} image={slides[3].image} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box title={slides[4].title} image={slides[4].image} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Box title={slides[5].title} image={slides[5].image} />
+            </SwiperSlide>
           </Swiper>
         </div>
         <SliderNavigation />
