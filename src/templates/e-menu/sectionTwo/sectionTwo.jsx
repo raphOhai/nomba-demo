3abc664dcc008d818891e48431719f5e0f01fe49
Johnexzy <obajohn75@gmail.com>
2023-06-13 08:35:43 +0100

    feat-menu

--- src/templates/e-menu/sectionTwo/sectionTwo.jsx
+++ src/templates/e-menu/sectionTwo/sectionTwo.jsx
@@ -7,15 +7,15 @@ import ctl from "@netlify/classnames-template-literals";
 const EmenuSection2 = () => {
   const { SIGNUP_URL } = constants;
   return (
-    <section bg>
+    <section class=" bg-n-yellow1">
       <Container className="mt-8 md:mt-[100px]">
         <div class="relative w-full">
-          <div className="absolute lg:left-[13.5rem] top-[3rem] max-w-[336px] md:max-w-[23rem]">
+          <div className="absolute lg:left-[13.5rem] top-[2rem] max-w-[336px] md:max-w-[23rem]">
             <Ntext variant="h1" color="primary-100">
               Power your Restaurant
             </Ntext>
           </div>
-          <div className="flex justify-center md:-ml-[12rem] -ml-[10rem] md:mt-[-5rem] -mt-[4rem]">
+          <div className="flex justify-center md:-ml-[12rem] -ml-[10rem] md:mt-[-5rem] -mt-[1rem]">
             <video src={heroVideo} controls={false} muted playsInline autoPlay loop width={793} height={990}></video>
           </div>
           <div class="absolute lg:right-[12rem] right-0 md:top-[57%] top-[70%] max-w-[290px] md:max-w-[21rem]">
