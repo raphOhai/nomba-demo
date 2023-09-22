58fa7bc1f4152f14fb1d4cb813d32952dce1447f
Bashir Hamza <bashir.hamza@nomba.com>
2023-09-22 13:06:16 +0100

    removed some vide files

---
+++ src/utils/videoLinks.js
@@ -0,0 +1,7 @@
+const videoLinks = {
+  Comp: "https://drake.kudi.ai/media/cac4f721-a083-4fd5-8faa-5f63b2687612",
+  Hero: "https://drake.kudi.ai/media/10ed51dd-03c4-41b2-8e1d-62a2ac707caf",
+  Video: "https://drake.kudi.ai/media/a848d490-f2b1-4305-9b6e-4f89fe3f3257",
+  Comp2: "https://drake.kudi.ai/media/b743028a-7449-4f21-85e0-4755cdc036dd",
+  Video2: "https://drake.kudi.ai/media/6809d194-b5d7-428f-8a9a-85468563e4ea",
+};
