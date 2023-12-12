96615754d5e451e3f29cac1f360de145b455e0c7
ugoostanleyibe <ugoostanleyibe@gmail.com>
2023-12-12 20:38:55 +0100

    resize advantages section

--- src/templates/checkout/advantages/advantages.jsx
+++ src/templates/checkout/advantages/advantages.jsx
@@ -1,48 +1,25 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React from "react";
 import ctl from "@netlify/classnames-template-literals";
 
 import { advantages } from "config/checkout";
 import { Ntext } from "components";
 
-gsap.registerPlugin(ScrollTrigger);
-
 const CheckoutAdvantages = () => {
-  const wrapperRef = useRef(null);
-
-  useEffect(() => {
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        end: 'bottom center',
-        trigger: wrapperRef.current,
-        start: 'top center',
-        scrub: 1,
-      },
-    });
-
-    advantages.forEach((_, index) => {
-      timeline.fromTo(
-        `.advantage-${index}`,
-        { opacity: 0, scale: 0.5 },
-        { opacity: 1, scale: 1, duration: 1 },
-        index * 2
-      );
-    });
-  }, []);
-
   return (
-    <section className={wrapperStyle} ref={wrapperRef}>
+    <section className={wrapperStyle}>
       <section className={contentStyle}>
-        {advantages.map((advantage, index) => (
-          <Ntext
-            key={advantage}
-            value={advantage}
-            color="primary-100"
-            className={`advantage-${index} !font-normal md:!text-[56px] md:!leading-[64px] !-tracking-[2px] max-w-[800px] text-center`}
-            variant="h2"
-          />
-        ))}
+        <Ntext
+          color="primary-100"
+          value={advantages[0]}
+          className="!font-normal md:!text-[56px] md:!leading-[64px] !-tracking-[2px] max-w-[800px] text-center"
+          variant="h2"
+        />
+        <Ntext
+          color="n-silver"
+          value={advantages[1]}
+          className="!font-normal md:!text-[56px] md:!leading-[64px] !-tracking-[2px] max-w-[800px] text-center"
+          variant="h2"
+        />
       </section>
     </section>
   );
@@ -51,21 +28,18 @@ const CheckoutAdvantages = () => {
 const wrapperStyle = ctl(`
   bg-checkout-advantages
   bg-[length:100%_100%]
-  advantages-wrapper
-  overflow-hidden
-  pb-[120px]
   w-screen
-  pt-40
+  h-screen
 `);
 
 const contentStyle = ctl(`
   flex
   flex-col
   items-center
-  space-y-[200px]
   justify-center
-  relative
+  space-y-[200px]
   w-full
+  h-full
 `);
 
 export { CheckoutAdvantages };
