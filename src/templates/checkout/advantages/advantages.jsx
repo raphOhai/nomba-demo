582e836308416eebdb45cbd732cb79ecb813f5eb
ugoostanleyibe <ugoostanleyibe@gmail.com>
2023-12-16 17:51:41 +0100

    improve animation of advantages

--- src/templates/checkout/advantages/advantages.jsx
+++ src/templates/checkout/advantages/advantages.jsx
@@ -1,108 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ctl from "@netlify/classnames-template-literals";
-import gsap from "gsap";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { advantages } from "config/checkout";
 import { Ntext } from "components";
+
+import { advantages } from "config/checkout";
+
+import RibbonsBackground from "svgs/checkout/advantages-ribbons.svg";
+import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const CheckoutAdvantages = () => {
-  const [firstIndex, setFirstIndex] = useState(0);
-  const [secondIndex, setSecondIndex] = useState(1);
-
   useEffect(() => {
-    const advantageText = document.querySelectorAll(".advantage-text");
-
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: "#advantages",
-        start: "top top",
-        end: "+=100%",
-        pin: true,
-        scrub: 1,
-      },
-    })
-      .to(advantageText, {
-        onComplete: () => {
-          setSecondIndex(2);
-          setFirstIndex(1);
-        },
-        opacity: 0,
-      })
-      .to(advantageText, {
-        onReverseComplete: () => {
-          setSecondIndex(1);
-          setFirstIndex(0);
-        },
-        opacity: 1,
-      })
-      .to(advantageText, {
-        onComplete: () => {
-          setSecondIndex(3);
-          setFirstIndex(2);
-        },
-        opacity: 0,
-      })
-      .to(advantageText, {
-        onReverseComplete: () => {
-          setSecondIndex(2);
-          setFirstIndex(1);
-        },
-        opacity: 1,
-      })
-      .to(advantageText, {
-        onComplete: () => {
-          setSecondIndex(4);
-          setFirstIndex(3);
-        },
-        opacity: 0,
-      })
-      .to(advantageText, {
-        onReverseComplete: () => {
-          setSecondIndex(3);
-          setFirstIndex(2);
-        },
-        opacity: 1,
+    gsap.utils.toArray(".advantage-text").forEach(text => {
+      ScrollTrigger.create({
+        start: "top center",
+        toggleClass: "!text-white",
+        trigger: text,
       });
+
+      gsap.timeline({
+        scrollTrigger: {
+          end: "bottom 160px",
+          start: "top 160px",
+          trigger: text,
+          scrub: 1,
+        },
+      }).to(text, { opacity: 0 });
+    });
   }, []);
 
   return (
     <section id="advantages" className={wrapperStyle}>
+      <RibbonsBackground className="absolute w-screen top-0" />
       <section className={contentStyle}>
-        <Ntext
-          color="primary-100"
-          value={advantages[firstIndex]}
-          className="advantage-text !font-normal md:!text-[56px] md:!leading-[64px] !-tracking-[2px] max-w-[800px] text-center"
-          variant="h2"
-        />
-        <Ntext
-          color="n-silver"
-          value={advantages[secondIndex]}
-          className="advantage-text !font-normal md:!text-[56px] md:!leading-[64px] !-tracking-[2px] max-w-[800px] text-center"
-          variant="h2"
-        />
+        {
+          advantages.map(advantage => (
+            <Ntext
+              color="n-silver"
+              value={advantage}
+              className="advantage-text !font-normal md:!text-[56px] md:!leading-[64px] !-tracking-[2px] max-w-[800px] text-center"
+              variant="h2"
+            />
+          ))
+        }
       </section>
     </section>
   );
 };
 
 const wrapperStyle = ctl(`
-  bg-checkout-advantages
-  bg-[length:100%_100%]
+  bg-n-grey8
+  h-[1656px]
   w-screen
-  h-screen
+  relative
 `);
 
 const contentStyle = ctl(`
   flex
   flex-col
+  pb-[120px]
   items-center
-  justify-center
   space-y-[200px]
-  w-full
-  h-full
+  justify-center
+  absolute
+  w-screen
+  top-0
+  pt-40
+  px-8
 `);
 
 export { CheckoutAdvantages };
