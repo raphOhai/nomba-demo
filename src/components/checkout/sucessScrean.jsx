acd416bf05d23e35325c2b374e4ea041bf0b274e
raphaelohai5 <sameleno6@gmail.com>
2024-01-13 18:29:18 +0100

    fixed

--- src/components/checkout/sucessScrean.jsx
+++ src/components/checkout/sucessScrean.jsx
@@ -4,22 +4,24 @@ import React, { useContext } from "react";
 import { AppContext } from "states/context";
 
 const SucessScrean = () => {
-  const { onOpen } = useContext(AppContext);
+  const { closeAndReset } = useContext(AppContext);
   return (
     <div className="sucess-box flex center">
-      <div className="stack gap-3">
-        <Ntext variant="h4" className="text-center">
-          <div style={{ color: "#fff" }}>Request sent!</div>
-        </Ntext>
-        <Ntext variant="text3" className="text-center">
-          <div style={{ color: "#717171" }}>
-            Thank you for your interest! Your demo request has been received We would get back to you as soon as
-            possible via your email or phone number.
-          </div>
-        </Ntext>
+      <div className="stack gap3">
+        <div className="stack gap-2">
+          <Ntext variant="h4" className="text-center">
+            <div style={{ color: "#fff" }}>Request sent!</div>
+          </Ntext>
+          <Ntext variant="text3" className="text-center">
+            <div style={{ color: "#717171" }}>
+              Thank you for your interest! Your demo request has been received We would get back to you as soon as
+              possible via your email or phone number.
+            </div>
+          </Ntext>
+        </div>
 
         <Button
-          onClick={onOpen}
+          onClick={closeAndReset}
           className="!font-medium  !text-center contact-btn2 responsive-btn btn2"
           text="Got it!"
         />
