e770a2c492da9198e0263be567b4b8fbfac6d6fe
raphaelohai5 <sameleno6@gmail.com>
2024-01-19 11:04:49 +0100

    fixed

--- src/pages/menu2.js
+++ src/pages/menu2.js
@@ -1,5 +1,5 @@
 import Layout from "components/layout-max";
-import React from "react";
+import React, { useEffect } from "react";
 import SeoConf from "config/seo/meta";
 // import { Container } from "components";
 import HeroUp from "templates/menueUpdate/hero/heroUp";
@@ -15,29 +15,46 @@ import { HomepageTipsAndUpdate } from "templates";
 import { Cart } from "components/checkout";
 import { faqData } from "config/e-menu";
 import { EmenuSection5, MenuSection6, EmenuTestimonial, EveryBusiness } from "templates/e-menu";
-const menu2 = () => {
+import { ReactLenis } from "@studio-freight/react-lenis";
+import { useRef } from "react";
+import gsap from "gsap";
+const Menu2 = () => {
   MultipleAnimator();
-  return (
-    <Layout title={SeoConf.menu.title} description={SeoConf.menu.description} useStickyNav={false}>
-      <HeroUp />
-      <Section1 />
-      <PictureCta />
-      <EmenuSection4Black
-        tools={businessTool}
-        headingText="Fully integrated tool for smoothly running your restaurant business"
-      />
-      <EmenuSection5 cards={howItworks} />
-      <EmenuTestimonial headingText="Why businesses are choosing Nomba" testimonials={eMenuTestimonial} />
-      <MenuSection6 />
-      <CommonQuestions questions={faqData} />
+  const lenisRef = useRef();
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.raf(time * 1000);
+    }
 
-      <EveryBusiness />
-      <HomepageTipsAndUpdate />
-      <div data-lenis-prevent>
-        <Cart title="Get Started With Menu" type="Menu" />
-      </div>
-    </Layout>
+    gsap.ticker.add(update);
+
+    return () => {
+      gsap.ticker.remove(update);
+    };
+  });
+  return (
+    <ReactLenis root ref={lenisRef} autoRaf={false}>
+      <Layout title={SeoConf.menu.title} description={SeoConf.menu.description} useStickyNav={false}>
+        <HeroUp />
+        <Section1 />
+        <PictureCta />
+        <EmenuSection4Black
+          tools={businessTool}
+          headingText="Fully integrated tool for smoothly running your restaurant business"
+        />
+        <EmenuSection5 cards={howItworks} />
+        <EmenuTestimonial headingText="Why businesses are choosing Nomba" testimonials={eMenuTestimonial} />
+        <MenuSection6 />
+        <CommonQuestions questions={faqData} />
+
+        <EveryBusiness />
+        <HomepageTipsAndUpdate />
+        <div data-lenis-prevent>
+          <Cart title="Get Started With Menu" type="Menu" />
+        </div>
+      </Layout>
+    </ReactLenis>
   );
 };
 
-export default menu2;
+export default Menu2;
