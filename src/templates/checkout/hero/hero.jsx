bfd55e82799cfed92026b8cadd4e02acdbeb98f2
raph <sameleno6@gmail.com>
2024-03-04 16:02:40 +0100

    fixed hero section animation

--- src/templates/checkout/hero/hero.jsx
+++ src/templates/checkout/hero/hero.jsx
@@ -8,7 +8,7 @@ import { heroSection } from "config/checkout";
 import RightBlur from "svgs/checkout/hero-right-blur.svg";
 import LeftBlur from "svgs/checkout/hero-left-blur.svg";
 import Dss from "svgs/checkout/dssCompliant.svg";
-
+import Weare from "svgs/weare.svg";
 import { AppContext } from "states/context";
 
 import "./reusables.scss";
@@ -23,72 +23,67 @@ const CheckoutPageHero = () => {
   });
 
   return (
-    <div className="stack gap1">
-      <div className={wrapperStyle}>
-        <Container>
-          <div className="stack gap4">
-            <Container className="grid1 gap2">
-              <section className="flex flex-col items-center lg:items-start max-w-[740px]">
-                <article className={titleBadgeStyle}>
-                  <Ntext color="b-6" value="Nomba Checkout" className="!font-medium" variant="text2" />
-                </article>
-                <Ntext
-                  value={title}
-                  color="primary-100"
-                  className="!font-medium md:!text-[64px] md:!leading-[72px] !-tracking-[2px] text-center lg:text-start min-h-[150px] mt-2.5"
-                  variant="h1"
+    <div className={wrapperStyle}>
+      <Container>
+        <div className="stack gap2">
+          <div className="grid1 gap2">
+            <section className="flex flex-col items-center lg:items-start max-w-[740px]">
+              <article className={titleBadgeStyle}>
+                <Ntext color="b-6" value="Nomba Checkout" className="!font-medium" variant="text2" />
+              </article>
+              <Ntext
+                value={title}
+                color="primary-100"
+                className="!font-medium md:!text-[64px] md:!leading-[72px] !-tracking-[2px] text-center lg:text-start min-h-[150px] mt-2.5"
+                variant="h1"
+              />
+              <Ntext
+                color="primary-600"
+                value={description}
+                className="md:!leading-[32px] !-tracking-[0.8px] text-center lg:text-start max-w-[628px] mt-6"
+                variant="text4lite"
+              />
+              <section className={buttonsStyle}>
+                <Button
+                  className="!font-medium !text-base !text-n-smoky !w-[76vw] lg:!w-[246px] !h-14 lg:z-10"
+                  text="Try Live Demo"
+                  onClick={onOpen}
                 />
-                <Ntext
-                  color="primary-600"
-                  value={description}
-                  className="md:!leading-[32px] !-tracking-[0.8px] text-center lg:text-start max-w-[628px] mt-6"
-                  variant="text4lite"
+                <Button
+                  text="Get Started"
+                  className="!bg-transparent !border-solid !border-n-charcoal !font-medium !text-base !text-white !w-[76vw] lg:!w-[204px] !h-14 contact-btn responsive-btn btn2"
+                  href={{ url: "https://dashboard.nomba.com/auth/sign-up-new" }}
                 />
-                <section className={buttonsStyle}>
-                  <Button
-                    className="!font-medium !text-base !text-n-smoky !w-[76vw] lg:!w-[246px] !h-14 lg:z-10"
-                    text="Try Live Demo"
-                    onClick={onOpen}
-                  />
-                  <Button
-                    text="Get Started"
-                    className="!bg-transparent !border-solid !border-n-charcoal !font-medium !text-base !text-white !w-[76vw] lg:!w-[204px] !h-14 contact-btn responsive-btn btn2"
-                    href={{ url: "https://dashboard.nomba.com/auth/sign-up-new" }}
-                  />
-                </section>
               </section>
+            </section>
+            <div className="end">
               <Rive
                 src="/check-out-with-nomba.riv"
                 layout={new Layout({ fit: Fit.Cover })}
                 className={animationWrapperStyle}
                 animations="Timeline 1"
               />
-            </Container>
+            </div>
+          </div>
 
-            <div className="stack gap5 riv-Box2">
-              <div className="end">
-                <div className="flex gap-3">
-                  <div className="center">
-                    <Ntext
-                      color="primary-600"
-                      value="We are ->"
-                      className="!-tracking-[0.8px] text-center lg:text-start max-w-[628px] "
-                      variant="p13"
-                    />
-                  </div>
+          <div className="stack gap5 riv-Box2">
+            <div className="end">
+              <div className="flex gap-3">
+                <div className="center">
+                  <Weare />
+                </div>
 
-                  <div className="center">
-                    <Dss />
-                  </div>
+                <div className="center">
+                  <Dss />
                 </div>
               </div>
             </div>
           </div>
-        </Container>
+        </div>
+      </Container>
 
-        <RightBlur className={rightBlurStyle} />
-        <LeftBlur className={leftBlurStyle} />
-      </div>
+      <RightBlur className={rightBlurStyle} />
+      <LeftBlur className={leftBlurStyle} />
     </div>
   );
 };
@@ -102,6 +97,7 @@ const animationWrapperStyle = ctl(`
   rounded-3xl
   h-[348px]
   w-full
+ 
 `);
 
 const titleBadgeStyle = ctl(`
